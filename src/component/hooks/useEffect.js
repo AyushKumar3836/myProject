ee81cdfb40c148c885fc28f044efb6e53c805c47
AYUSH3836 <93387454+AYUSH3836@users.noreply.github.com>
2023-02-13 12:27:11 +0530

    first commit

---
+++ src/component/hooks/useEffect.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+
+const UseEffect = () => {
+
+   const  initialData=15; 
+
+    const [myNum,setMyNum] = useState(initialData);
+
+     //console.log(myNum);
+
+
+     useEffect(() => {
+      document.title=`Chats(${myNum})`;
+     });
+
+    return (
+        <>
+         <div className="center_div">
+            <p>{myNum}</p>
+            <div class="button2"  onClick={() => setMyNum(myNum+1)}>
+               <span></span>
+               <span></span>
+               <span></span>
+               <span></span>
+               INCR
+            </div>
+            
+         </div>
+        </>
+    );
+
+}
+
+export default UseEffect;
